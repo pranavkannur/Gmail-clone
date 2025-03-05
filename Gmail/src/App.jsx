6aee089c045f5b44c057a31a80ddaaa9cb269706
pranavkannur <kannurpranav67@gmail.com>
2025-03-05 15:00:39 +0530

    test

--- Gmail/src/App.jsx
+++ Gmail/src/App.jsx
@@ -1,10 +1,11 @@
 
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Gmail Clone</h1>
+    <div>
+      <Navbar/>
     </div>
   ) 
       
